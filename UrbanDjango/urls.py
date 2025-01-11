33bd79b234edd2c867c64bfaf525a4b3aa571b54
AlexanderLyuft <alexanderlyuft@mail.ru>
2025-01-12 02:40:10 +0700

    Добавлено приложение регистрации task5

--- UrbanDjango/urls.py
+++ UrbanDjango/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path#, include
-from task4 import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('games/', views.games, name='games'),
-    path('cart/', views.cart, name='cart'),
+    path('registration/', include('task5.urls')),
+    path('', include('task5.urls')),
 ]
